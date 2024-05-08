5b2fd76b7a8833b81643cb9d2b9519cbdeadf26f
Yeyun Lu <luyeyun18826077098@outlook.com>
2024-05-07 19:35:36 -0700

    Prepare for deployment

--- src/main/java/com/sanrenxing/service/security/config/CorsConfig.java
+++ src/main/java/com/sanrenxing/service/security/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         var corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000"));
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000", "https://san-ren-xing-console-c0666bedcf9c.herokuapp.com"));
         corsConfiguration.setAllowedHeaders(List.of("Content-Type", "Authorization", "type", "Cache-Control"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         source.registerCorsConfiguration("/**", corsConfiguration);
