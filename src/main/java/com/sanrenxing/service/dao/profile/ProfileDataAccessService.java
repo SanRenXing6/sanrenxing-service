8552e274bdf089d3fef267b254f046593b1b06a1
Yeyun Lu <luyeyun18826077098@outlook.com>
2024-05-16 22:20:54 -0700

    Fix null value issue for fetching profiles

--- src/main/java/com/sanrenxing/service/dao/profile/ProfileDataAccessService.java
+++ src/main/java/com/sanrenxing/service/dao/profile/ProfileDataAccessService.java
@@ -47,14 +47,20 @@ public class ProfileDataAccessService implements ProfileDao {
         final String sql = "SELECT * FROM profiles;";
         return jdbcTemplate.query(sql,
                 (resultSet, i) -> {
+
+                    UUID imageId = null;
                     UUID id = UUID.fromString(resultSet.getString("id"));
                     UUID userId = UUID.fromString(resultSet.getString("userId"));
                     String description = resultSet.getString("description");
-                    UUID imageId = UUID.fromString(resultSet.getString("imageId"));
+                    String imageString = resultSet.getString("imageId");
                     int rate = resultSet.getInt("rate");
                     String needs = resultSet.getString("needs");
                     List<Skill> skills = JsonConverter.deserialize(resultSet.getString("skills"), Skill.class);
                     List<Feedback> feedbacks = JsonConverter.deserialize(resultSet.getString("feedbacks"), Feedback.class);
+
+                    if (imageString!=null && !imageString.isEmpty()) {
+                        imageId=UUID.fromString(imageString);
+                    }
                    return new Profile(id, userId, description, imageId, rate, needs, skills, feedbacks);
                 });
     }
